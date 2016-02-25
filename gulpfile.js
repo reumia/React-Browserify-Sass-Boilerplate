228abc86ea7f141aa5e0d6b02fd3f89365cc8e47
zzoman <reumia@gmail.com>
2016-02-25 15:17:28 +0900

    Initial

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp'),
+	browserify = require('browserify'),
+	watchify = require('watchify'),
+	webserver = require('gulp-webserver'),
+	sass = require('gulp-sass'),
+	source = require('vinyl-source-stream'),
+	sourcemaps  = require('gulp-sourcemaps');
+
+gulp.task('browserify', function () {
+    watchify(browserify('./src/js/app.js'))
+        .bundle()
+        .on('error', function(err) {
+            console.error(err.message);
+        })
+        .pipe(source('app.js'))
+        .pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('sass', function () {
+  return gulp.src('./src/scss/app.scss')
+  	.pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('/map'))
+    .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('default', ['watch']);
+
+gulp.task('watch', function () {
+    gulp.src('./')
+        .pipe(webserver({
+            livereload: false,
+            port: 9999,
+            fallback: './app.html'
+        }));
+    gulp.start('browserify');
+    gulp.watch('./src/js/*.js', ['default']);
+    gulp.watch('./src/scss/**/*.scss', ['sass']);
+});
